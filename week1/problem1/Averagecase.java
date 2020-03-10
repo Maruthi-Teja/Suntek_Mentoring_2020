2ebbb8313cea908795563b3d2def33bd97baa9fd
mentor2.suntek <mentor2.suntek@gmail.com>
2020-03-10 11:53:10 +0530

    review comments

--- week1/problem1/Averagecase.java
+++ week1/problem1/Averagecase.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class problem1 {
 	// function for bit manipulation
-           static int[] Xor(int[] array,int[] queries)
+           static int[] Xor(int[] array,int[] queries)		//dont use static.
+		   						//Create the class obj and call the func with obj reference
+		   						// Change function name to more appropriate name
            {
         	   int arr[] =new int[queries.length/2];
         	   int c=0;
         	   
-        	   for(int i=0;i<queries.length;i+=2)
+        	   for(int i=0;i<queries.length;i+=2)		//create a variable for queries.length instead of calling the same func multiple times
         	   {
         		   int result=0;
         		   for(int j=queries[i];j<=queries[i+1];j++)
@@ -18,12 +20,14 @@ public class problem1 {
         		   }
         		   arr[c++]=result;
         	   }
-        		   return arr;
+        		   return arr;				// missed indentation
            }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                    int[][] input = {{1,3,4,8},{1,8,0,1,3,4},{0,0,0,0,0,0,0},{1,1,1,1,1},{100,0,200,45}};
-                   int[][] queries = {{0,1,1,2,0,3,3,3},{0,0,0,4,1,4,4,4,4,5},{0,3,0,2,0,5,1,5},{0,3,0,2,0,4,1,2},{0,3,1,3,3,3,2,3,1,2}};
+                   int[][] queries = {{0,1,1,2,0,3,3,3},{0,0,0,4,1,4,4,4,4,5},
+				      {0,3,0,2,0,5,1,5},{0,3,0,2,0,4,1,2},
+				      {0,3,1,3,3,3,2,3,1,2}};    			//Queries format is wrong. check the question again
                    int[][] output =new int[input.length][];
                    int[][] expected_out = {{2,7,14,8},{1,11,10,3,7},{0,1,0,0},{0,1,1,0},{129,229,45,229,200}};
                   
@@ -31,7 +35,7 @@ public class problem1 {
                    {
                 	   output[i]= Xor(input[i],queries[i]);
                    }
-                   Testing();
+                   Testing();								//NO function declaration yet u r using it
                 //Testing
                    for(int i=0;i<expected_out.length;i++)
                    {
